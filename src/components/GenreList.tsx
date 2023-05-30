0f61cd53cd3e939dd1f8200183a22553b7532506
Dion Melosantos <6504404+kurabemono@users.noreply.github.com>
2023-05-30 23:52:55 +0900

    Ch8-20 Reimplemented with Skeleton

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,15 +1,28 @@
-import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
+import {
+  HStack,
+  Image,
+  List,
+  ListItem,
+  SkeletonText,
+  Text,
+} from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data, isLoading, error } = useGenres();
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
   if (error) return null;
-  if (isLoading) return <Spinner />;
 
   return (
     <List>
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <ListItem key={skeleton}>
+            <SkeletonText mt="4" noOfLines={1} skeletonHeight="8" />
+          </ListItem>
+        ))}
       {data.map((genre) => (
         <ListItem key={genre.id} paddingY="5px">
           <HStack>
