76d150c6942eecaa34c88af15bb585740357c56f
Dion Melosantos <6504404+kurabemono@users.noreply.github.com>
2023-05-31 13:16:46 +0900

    Ch8-27 Implemented game sorting

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,5 @@
 const getCroppedImageUrl = (url: string) => {
+  if (!url) return "";
   const searchString = "media/";
   const index = url.indexOf(searchString) + searchString.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
