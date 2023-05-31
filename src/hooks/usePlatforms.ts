85e5565067f0a7720f3c6c3ba84732b351e3489d
Dion Melosantos <6504404+kurabemono@users.noreply.github.com>
2023-05-31 13:43:14 +0900

    Ch8-32 Add dynamic page heading

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -6,6 +6,6 @@ interface Platform {
   slug: string;
 }
 
-const usePLatforms = () => useData<Platform>("platforms/lists/parents");
+const usePlatforms = () => useData<Platform>("platforms/lists/parents");
 
-export default usePLatforms;
+export default usePlatforms;
