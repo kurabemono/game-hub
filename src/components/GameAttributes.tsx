db415b262f49b561a8cc195abce290871a87e775
Dion Melosantos <6504404+kurabemono@users.noreply.github.com>
2023-06-29 16:39:43 +0900

    Ch4-18 Build game attributes

---
+++ src/components/GameAttributes.tsx
@@ -0,0 +1,35 @@
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import { Game } from "../entities/Game";
+import AttributeContainer from "./AttributeContainer";
+import CriticScore from "./CriticScore";
+
+interface Props {
+  game: Game;
+}
+
+const GameAttributes = ({ game }: Props) => {
+  return (
+    <SimpleGrid columns={2} as="dl">
+      <AttributeContainer term="Platforms">
+        {game.parent_platforms?.map(({ platform }) => (
+          <Text key={platform.id}>{platform.name}</Text>
+        ))}
+      </AttributeContainer>
+      <AttributeContainer term="Metascore">
+        <CriticScore score={game.metacritic} />
+      </AttributeContainer>
+      <AttributeContainer term="Genres">
+        {game.genres.map((genre) => (
+          <Text key={genre.id}>{genre.name}</Text>
+        ))}
+      </AttributeContainer>
+      <AttributeContainer term="Publishers">
+        {game.publishers?.map((publisher) => (
+          <Text key={publisher.id}>{publisher.name}</Text>
+        ))}
+      </AttributeContainer>
+    </SimpleGrid>
+  );
+};
+
+export default GameAttributes;
